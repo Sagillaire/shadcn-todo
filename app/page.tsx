3df8ef2f24faa50c10933bae62b6cb5217ce6397
Sagillaire <Jasantise@outlook.es>
2023-09-26 22:52:46 -0500

    Feature: mode-toggle component

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
+import { ModeToggle } from '@/components/mode-toggle/mode-toggle'
 import { Button } from '@/components/ui/button'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <ModeToggle />
       <Button>Submit</Button>
     </main>
   )
